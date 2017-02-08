b4a3c8871e5a4a3bed1f5c1db966ca635a9ed515
you21979 <you2197901@gmail.com>
2017-02-08 14:44:11 +0900

    mod

--- example/ex1.js
+++ example/ex1.js
@@ -1,2 +1,7 @@
-var rev = require('..')
-rev.match("216.58.220.238", "1e100.net$").then(console.log)
+const rev = require('..')
+rev.reverse_lookup("216.58.220.238").
+    then(hosts =>
+        hosts.map(host =>
+            ({host:host,check:host.match("1e100.net$") ? true : false })
+        )
+    ).then(console.log)
--- index.js
+++ index.js
@@ -2,22 +2,29 @@
 var dns = require('dns');
 var Promise = require('bluebird');
 
-var lookup = exports.lookup = function(ipaddress){
+var lookup_ipv4 = exports.lookup_ipv4 = function(hostname){
     return new Promise(function(resolve, reject){
-        dns.reverse(ipaddress, function (err, hostnames) {
+        dns.resolve4(hostname, function (err, ipaddresses) {
             if(err) reject(err)
-            else resolve(hostnames)
+            else resolve(ipaddresses)
         })
     })
 }
 
-var match = exports.match = function(ipaddress, str){
-    return lookup(ipaddress).then(function(res){
-        return res.map(function(r){
-            return {
-                host : r,
-                match : r.match(str) ? true : false,
-            }
+var lookup_ipv6 = exports.lookup_ipv6 = function(hostname){
+    return new Promise(function(resolve, reject){
+        dns.resolve6(hostname, function (err, ipaddresses) {
+            if(err) reject(err)
+            else resolve(ipaddresses)
+        })
+    })
+}
+
+var reverse_lookup = exports.reverse_lookup = function(ipaddress){
+    return new Promise(function(resolve, reject){
+        dns.reverse(ipaddress, function (err, hostnames) {
+            if(err) reject(err)
+            else resolve(hostnames)
         })
     })
 }
